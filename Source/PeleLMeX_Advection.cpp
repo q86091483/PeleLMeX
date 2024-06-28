2ae20f2b857b55cc40e24adad202f8c72e06f786
zisen <zisen347@gmail.com>
2024-06-28 19:26:20 -0400

    Zero diffusion update, non zero forcing for aux.

--- Source/PeleLMeX_Advection.cpp
+++ Source/PeleLMeX_Advection.cpp
@@ -347,7 +347,7 @@ PeleLM::getScalarAdvForce_Aux(
   #if (NUMAGE > 0)
           rhs_age = old_arr(i,j,k,AGE) / (old_arr(i,j,k,MIXF) + 1E-8);
           rhs_age *= rhs_mixf;
-          rhs_age = 0.0;
+          //rhs_age = 0.0;
           fAux(i,j,k,NUMMIXF) = rhs_age;
     #if (NUMAGEPV > 0)
           fAux(i,j,k,NUMMIXF+NUMAGE) = rhs_age;
@@ -356,7 +356,7 @@ PeleLM::getScalarAdvForce_Aux(
   #if (NUMAGE > 1)
           rhs_age = old_arr(i,j,k,AGE+1) / (old_arr(i,j,k,MIXF+1) + 1E-8);
           rhs_age *= -rhs_mixf;
-          rhs_age = 0.0;
+          //rhs_age = 0.0;
           fAux(i,j,k,NUMMIXF+1) = rhs_age;
     #if (NUMAGEPV > 1)
           fAux(i,j,k,NUMMIXF+NUMAGE+1) = rhs_age;
@@ -1269,7 +1269,7 @@ PeleLM::updateScalarAux(
 #if (NUMAGE > 0)
           rhs_age = old_arr(i,j,k,AGE) / (old_arr(i,j,k,MIXF) + 1E-3);
           rhs_age *= rhs_mixf;
-          //rhs_age = 0.0;
+          rhs_age = 0.0;
           new_arr(i,j,k,AGE) += dt * rhs_age;
   #if (NUMAGEPV > 0)
           new_arr(i,j,k,AGEPV) += dt * rhs_age;
@@ -1278,7 +1278,7 @@ PeleLM::updateScalarAux(
 #if (NUMAGE > 1)
           rhs_age = old_arr(i,j,k,AGE+1) / (old_arr(i,j,k,MIXF+1) + 1E-3);
           rhs_age *= -rhs_mixf;
-          //rhs_age = 0.0;
+          rhs_age = 0.0;
           new_arr(i,j,k,AGE+1) += dt * rhs_age;
   #if (NUMAGEPV > 1)
           new_arr(i,j,k,AGEPV+1) += dt * rhs_age;
