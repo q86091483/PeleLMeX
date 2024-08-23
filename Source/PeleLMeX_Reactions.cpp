417ca70b99dbc21bccdaaab3dd565a28619aa793
zisen <zisen347@gmail.com>
2024-08-23 10:57:53 -0400

    remove output

--- Source/PeleLMeX_Reactions.cpp
+++ Source/PeleLMeX_Reactions.cpp
@@ -496,8 +496,7 @@ PeleLM::getScalarReactForce(
               fact_Bilger[m] / (Zfu_lcl - Zox_lcl);
           }
           //fAux(i, j, k, 0) = a_of_s(i, j, k, MIXF); //  + 1.0 * rhs_mixf;
-          new_arr(i, j, k, MIXF+0) = 0.4;
-          amrex::Print() << MIXF << std::endl;
+          new_arr(i, j, k, MIXF+0) = 1.5;
           //  dt * (a_of_s(i, j, k, MIXF) + 1.0 * rhs_mixf);
 #if (NUMMIXF > 1)
           //fAux(i, j, k, 1) = a_of_s(i, j, k, MIXF+1) - 1.0 * rhs_mixf;
