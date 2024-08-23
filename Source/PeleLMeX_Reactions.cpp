52a6370cf15f9051e3fd78f89788e316e856a728
zisen <zisen347@gmail.com>
2024-08-23 10:34:32 -0400

    Remove MIXF+1.

--- Source/PeleLMeX_Reactions.cpp
+++ Source/PeleLMeX_Reactions.cpp
@@ -498,7 +498,7 @@ PeleLM::getScalarReactForce(
           fAux(i, j, k, 0) = a_of_s(i, j, k, MIXF); //  + 1.0 * rhs_mixf;
           new_arr(i, j, k, MIXF+0) = old_arr(i, j, k, MIXF+0) + dt * fAux(i, j, k, 0);
 #if (NUMMIXF > 1)
-          fAux(i, j, k, 1) = a_of_s(i, j, k, MIXF+1) - 1.0 * rhs_mixf;
+          //fAux(i, j, k, 1) = a_of_s(i, j, k, MIXF+1) - 1.0 * rhs_mixf;
 #endif
         }); // ParallelFor
 #endif
