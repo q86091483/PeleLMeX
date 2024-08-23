511e65c5b64399c04d53471a4ec09e293b52d420
zisen <zisen347@gmail.com>
2024-08-23 10:38:58 -0400

    Remove fAux

--- Source/PeleLMeX_Reactions.cpp
+++ Source/PeleLMeX_Reactions.cpp
@@ -495,8 +495,9 @@ PeleLM::getScalarReactForce(
             rhs_mixf += 0.5 * (dn(i, j, k, m) + dnp1(i, j, k, m)) *
               fact_Bilger[m] / (Zfu_lcl - Zox_lcl);
           }
-          fAux(i, j, k, 0) = a_of_s(i, j, k, MIXF); //  + 1.0 * rhs_mixf;
-          new_arr(i, j, k, MIXF+0) = old_arr(i, j, k, MIXF+0) + dt * fAux(i, j, k, 0);
+          //fAux(i, j, k, 0) = a_of_s(i, j, k, MIXF); //  + 1.0 * rhs_mixf;
+          new_arr(i, j, k, MIXF+0) = old_arr(i, j, k, MIXF+0) +
+            dt * (a_of_s(i, j, k, MIXF) + 1.0 * rhs_mixf);
 #if (NUMMIXF > 1)
           //fAux(i, j, k, 1) = a_of_s(i, j, k, MIXF+1) - 1.0 * rhs_mixf;
 #endif
