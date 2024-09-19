77d235aca3f4ee2d419dc541421491da2189e448
zisen <zisen347@gmail.com>
2024-09-19 17:35:31 -0400

    Force species output.

--- Source/PeleLMeX_Advance.cpp
+++ Source/PeleLMeX_Advance.cpp
@@ -480,10 +480,8 @@ void PeleLM::WriteDebugStateFile(int istep, int isdc) {
                , plt_VarsName.push_back("z_velocity"));
     if (m_incompressible == 0) {
       plt_VarsName.push_back("density");
-      if (m_plotStateSpec != 0) {
-        for (int n = 0; n < NUM_SPECIES; n++) {
-          plt_VarsName.push_back("rho.Y(" + names[n] + ")");
-        }
+      for (int n = 0; n < NUM_SPECIES; n++) {
+        plt_VarsName.push_back("rho.Y(" + names[n] + ")");
       }
       plt_VarsName.push_back("rhoh");
       plt_VarsName.push_back("temp");
