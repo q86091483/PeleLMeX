2fd164a352e480ae66eafb0dce453f18f568fa31
zisen <zisen347@gmail.com>
2025-08-28 10:29:53 +0200

    Add EB_average_down for auxiliary in averageDownScalars.

--- Source/PeleLMeX.cpp
+++ Source/PeleLMeX.cpp
@@ -366,16 +366,23 @@ PeleLM::averageDownScalars(const PeleLM::TimeStamp& a_time)
     EB_average_down(
       ldataFine_p->state, ldataCrse_p->state, DENSITY, nScal,
       refRatio(lev - 1));
+
+  #if (defined PELE_USE_AUX) && (NUMAUX > 0)
+    EB_average_down(
+      ldataFine_p->state, ldataCrse_p->state, FIRSTAUX, NUMAUX,
+      refRatio(lev - 1));
+  #endif
+
 #else
     average_down(
       ldataFine_p->state, ldataCrse_p->state, DENSITY, nScal,
       refRatio(lev - 1));
 
-#if (defined PELE_USE_AUX) && (NUMAUX > 0)
+  #if (defined PELE_USE_AUX) && (NUMAUX > 0)
     average_down(
       ldataFine_p->state, ldataCrse_p->state, FIRSTAUX, NUMAUX,
       refRatio(lev - 1));
-#endif
+  #endif
 
 #endif
   }
